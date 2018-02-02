514d7f602a2ca26d9bd1b54a3cb5bae3128cc1dd
Harikrishnan V <39754@hexaware.com>
2018-02-02 15:05:00 +0530

    added changes

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ app.post('/pioneerServiceNow', (req, res) =>{
   console.log(req.body);
   if(req.body.result.action === 'create_incident_category1'){    
       var resObj= {
-        "speech": "Hi "+req.body.result.parameters.userName +", Your Incident has been raised successfully.Please find the details below.\\n"
-        +"Description : "+req.body.result.parameters.issueDescription+". A acknowledge SMS will be sent to you with incident id to your Phone number"+req.body.result.parameters.phoneNumber,
+        "speech": "Hi "+req.body.result.parameters.userName +", Your Incident has been raised successfully. Please find the details below.\n"
+        +"Description : "+req.body.result.parameters.issueDescription+". A acknowledge SMS will be sent to you with incident id to your Phone number "+req.body.result.parameters.phoneNumber,
         "displayText": "result",
         "data": {"result": "result"},
         "source": "dialogflow"
