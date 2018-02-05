c35e4128604576218ae56ec3e07b19f830309d37
Harikrishnan V <39754@hexaware.com>
2018-02-05 16:22:32 +0530

    committed api files

--- app.js
+++ app.js
@@ -99,8 +99,8 @@ app.post('/pioneerServiceNow', (req, res) =>{
   }
   if(req.body.result.action === 'incident_status'){  
     serviceNowApi.getIncident(req.body.result.parameters.incidentId,function(err,data) {
-      console.log('After call',data)
-      if (data.error.message == 'No Record found'){
+      console.log('After call',data.message)
+      /*if (data.error.message == 'No Record found'){
         var resObj= {
           "speech": "Hi,Your Incident id is : "+req.body.result.parameters.incidentId + ",Not Found",
           "displayText": "result",
@@ -115,9 +115,9 @@ app.post('/pioneerServiceNow', (req, res) =>{
           "data": {"result": "result"},
           "source": "dialogflow"
         }
-      }
+      }*/
       
-      res.send(resObj)
+      //res.send(resObj)
     } );
     
   }
