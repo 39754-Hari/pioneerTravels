88d70c2c0de122280eb0975fc750db8a2dd28221
Harikrishnan V <39754@hexaware.com>
2018-02-05 12:01:50 +0530

    committed api files

--- app.js
+++ app.js
@@ -9,13 +9,13 @@ app.post('/pioneerServiceNow', (req, res) =>{
   console.log(req.body);
   if(req.body.result.action === 'create_incident_sub_category1-1'){    
     serviceNowApi.createIncident(req.body.result.parameters.userName,req.body.result.parameters.issueDescription,'2',function(err,data){
-      resObj= {
+      res.send({
         "speech": "Hi "+req.body.result.parameters.userName +", Your Incident has been raised successfully. Please find the details below.\n"
         +"Incident Id : "+data.result.number+". A acknowledge SMS will be sent to you with incident id to your Phone number "+req.body.result.parameters.phoneNumber,
         "displayText": "result",
         "data": {"result": "result"},
         "source": "dialogflow"
-      }   
+      } );  
     });   
   } 
   if(req.body.result.action === 'create_incident_sub_category1-2'){    
