5c95f2fd2c89335ed11ea4f95c8d227054acf3e6
Harikrishnan V <39754@hexaware.com>
2018-02-06 11:01:01 +0530

    committed chnaged files

--- app.js
+++ app.js
@@ -2,28 +2,36 @@ const express = require('express')
 const app = express()
 const bodyparser = require('body-parser')
 var serviceNowApi = require('./serviceNowApi.js')
-var resObj = {};
 app.use(bodyparser.json());
 
 app.post('/pioneerServiceNow', (req, res) =>{ 
   console.log(req.body);
-  if(req.body.result.action === 'create_incident_sub_category1-1'){    
-    //serviceNowApi.createIncident(req.body.result.parameters.userName,req.body.result.parameters.issueDescription,'2',function(err,data){
-      /*var resObj = {
-        "speech": "Hi "+req.body.result.parameters.userName +", Your Incident has been raised successfully and our service agent will get back to you within 48 hours regarding your issue. Please note your incident id for future reference : "+data.result.number
-        +". \nAn acknowledgement SMS will be sent to you with incident id to your Phone number "+req.body.result.parameters.phoneNumber
-        +".\nFor any queries regarding your issue, please call our customer care center.",
-        "displayText": "result",
-        "data": {"result": "result"},
-        "source": "dialogflow"
-      };*/
-      var resObj={
+  if(req.body.result.action === 'create_incident_sub_category1-1'){   
+    var resObj = {}; 
+    serviceNowApi.createIncident(req.body.result.parameters.userName,req.body.result.parameters.issueDescription,'2',function(err,data){
+      if (err) {
+        resObj={
+          "speech": "",
+          "messages": [
+            {
+              "type": 2,
+              "platform": "facebook",
+              "title": "Sorry! There was an error while processing your request. Please try again",
+              "replies": [
+                "Main menu"
+              ]
+            }
+          ]
+        };
+      }
+    else{
+      resObj={
       "speech": "",
       "messages": [
         {
           "type": 2,
           "platform": "facebook",
-          "title": "Hi "+req.body.result.parameters.userName +", Your Incident has been raised successfully and our service agent will get back to you within 48 hours regarding your issue. Please note your incident id for future reference : 12345"/*+data.result.number*/
+          "title": "Hi "+req.body.result.parameters.userName +", Your Incident has been raised successfully and our service agent will get back to you within 48 hours regarding your issue. Please note your incident id for future reference : "+data.result.number
           +". \nAn acknowledgement SMS will be sent to you with incident id to your Phone number "+req.body.result.parameters.phoneNumber
           +".\nFor any queries regarding your issue, please call our customer care center. What do you want to do next?",
           "replies": [
@@ -31,12 +39,13 @@ app.post('/pioneerServiceNow', (req, res) =>{
             "Main menu"
           ]
         }
-      ]
-    };
+        ]
+      };
+    }
       console.log('response : ',res);
       console.log('resobj : ',resObj);
       res.json(resObj);
-    //});   
+    });   
   } 
   if(req.body.result.action === 'create_incident_sub_category1-2'){    
     serviceNowApi.createIncident(req.body.result.parameters.userName,req.body.result.parameters.issueDescription,'2',function(err,data){
@@ -113,29 +122,44 @@ app.post('/pioneerServiceNow', (req, res) =>{
       res.send(resObj);
     }); 
   }
-  if(req.body.result.action === 'incident_status'){  
+  if(req.body.result.action === 'incident_status'){
+    var resObj = {}  ;
     serviceNowApi.getIncident(req.body.result.parameters.incidentId,function(err,data) {
-      console.log('After call',data.error)
-      if (data.error.message != 'undifiend' && data.error.message == 'No Record found'){
-        var resObj= {
+      if (err) {
+        resObj={
+          "speech": "",
+          "messages": [
+            {
+            "type": 2,
+            "platform": "facebook",
+            "title": "Sorry! There was an error while processing your request. Please try again",
+            "replies": [
+              "Main menu"
+            ]
+            }
+          ]
+        };
+      }
+      else{
+        if (data.error != 'undifiend' && data.error.message == 'No Record found'){
+          resObj= {
           "speech": "Hi,Your Incident id is : "+req.body.result.parameters.incidentId + ",Not Found",
           "displayText": "result",
           "data": {"result": "result"},
           "source": "dialogflow"
+          }
         }
-      }
-      else{
-        var resObj= {
+        else{
+         resObj= {
           "speech": "Hi,Your Incident id is : "+req.body.result.parameters.incidentId,
           "displayText": "result",
           "data": {"result": "result"},
           "source": "dialogflow"
+          }
         }
-      }
-      
-      //res.send(resObj)
-    } );
-    
+      }      
+      res.json(resObj);
+    });    
   }
 });
 
