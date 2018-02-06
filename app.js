cfdee358019af1a1f26eac30c717faa2d6a0451c
Harikrishnan V <39754@hexaware.com>
2018-02-06 10:40:05 +0530

    committed chnaged files

--- app.js
+++ app.js
@@ -8,19 +8,33 @@ app.use(bodyparser.json());
 app.post('/pioneerServiceNow', (req, res) =>{ 
   console.log(req.body);
   if(req.body.result.action === 'create_incident_sub_category1-1'){    
-    serviceNowApi.createIncident(req.body.result.parameters.userName,req.body.result.parameters.issueDescription,'2',function(err,data){
-      var resObj = {
+    //serviceNowApi.createIncident(req.body.result.parameters.userName,req.body.result.parameters.issueDescription,'2',function(err,data){
+      /*var resObj = {
         "speech": "Hi "+req.body.result.parameters.userName +", Your Incident has been raised successfully and our service agent will get back to you within 48 hours regarding your issue. Please note your incident id for future reference : "+data.result.number
         +". \nAn acknowledgement SMS will be sent to you with incident id to your Phone number "+req.body.result.parameters.phoneNumber
         +".\nFor any queries regarding your issue, please call our customer care center.",
         "displayText": "result",
         "data": {"result": "result"},
         "source": "dialogflow"
-      };
+      };*/
+      var resObj={
+      "speech": "",
+      "messages": [
+        {
+          "type": 2,
+          "platform": "facebook",
+          "title": "Hi,Welcome to Pioneer Travels ServiceNow from node. Choose an option to continue",
+          "replies": [
+            "Raise a complaint",
+            "Complaint Status"
+          ]
+        }
+      ]
+    };
       console.log('response : ',res);
       console.log('resobj : ',resObj);
-      res.send(resObj);
-    });   
+      res.json(resObj);
+    //});   
   } 
   if(req.body.result.action === 'create_incident_sub_category1-2'){    
     serviceNowApi.createIncident(req.body.result.parameters.userName,req.body.result.parameters.issueDescription,'2',function(err,data){
