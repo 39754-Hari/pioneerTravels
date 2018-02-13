d5064558c4cfd03258d8abe90195542b884cae44
Harikrishnan V <39754@hexaware.com>
2018-02-13 14:57:35 +0530

    committed file changes

--- googleAssitant.js
+++ googleAssitant.js
@@ -4,9 +4,21 @@ var googleAssistantFunction={};
 
 googleAssistantFunction.operation = function(req,res){
     const app = new ActionsSdkApp({ request: req, response: res });
-    app.ask({        
+    var resObj = {}; 
+    resObj={
+        "speech": "",
+        "messages": [
+          {            
+          "type": "simple_response",
+          "platform": "google",
+          "textToSpeech": "Hi,Welcome to Pioneer Travels ServiceNow. Choose an option to continue"
+          }
+        ]
+      };
+      return res.json(resObj);
+    /*app.ask({        
     speech: 'Howdy! You reached correct place',
     displayText: 'Howdy! welcome '
-    });
+    });*/
 }
 module.exports = googleAssistantFunction;
